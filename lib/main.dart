a76f80db35393dba1c3ae3d10a15134a9efa7f01
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-10-01 17:20:53 +0700

    Create UI My Cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(App());
 
@@ -187,6 +189,8 @@ class WidgetDescription extends StatelessWidget {
                           ),
                         ),
                         WidgetChooseColor(),
+                        SizedBox(height: 16.0),
+                        WidgetChooseSize(),
                         _buildWidgetProductInfo(context),
                       ],
                     ),
@@ -363,6 +367,81 @@ class _WidgetChooseColorState extends State<WidgetChooseColor> {
   }
 }
 
+class WidgetChooseSize extends StatefulWidget {
+  final List<String> listSize = [
+    'XL',
+    'S',
+    'M',
+    'L',
+  ];
+
+  @override
+  _WidgetChooseSizeState createState() => _WidgetChooseSizeState();
+}
+
+class _WidgetChooseSizeState extends State<WidgetChooseSize> {
+  int _indexSelected;
+
+  @override
+  void initState() {
+    _indexSelected = widget.listSize.length - 1;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'Size: ',
+                style: TextStyle(color: Colors.black87),
+              ),
+              TextSpan(
+                text: widget.listSize[_indexSelected],
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: SizedBox(
+            height: 24.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              physics: NeverScrollableScrollPhysics(),
+              reverse: true,
+              itemCount: widget.listSize.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: <Widget>[
+                    Text(widget.listSize[index]),
+                    Radio(
+                      value: index,
+                      groupValue: _indexSelected,
+                      activeColor: Color(0xFF32312D),
+                      onChanged: (index) {
+                        setState(() {
+                          _indexSelected = index;
+                        });
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class WidgetAddToBag extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -377,7 +456,15 @@ class WidgetAddToBag extends StatelessWidget {
           topRight: Radius.circular(36.0),
         ),
         child: InkWell(
-          onTap: () {},
+          onTap: () {
+            showBottomSheet(
+              context: context,
+              backgroundColor: Colors.transparent,
+              builder: (context) {
+                return WidgetMyCart();
+              },
+            );
+          },
           child: Stack(
             children: <Widget>[
               Align(
@@ -400,4 +487,264 @@ class WidgetAddToBag extends StatelessWidget {
       ),
     );
   }
+}
+
+class WidgetMyCart extends StatefulWidget {
+  @override
+  _WidgetMyCartState createState() => _WidgetMyCartState();
+}
+
+class _WidgetMyCartState extends State<WidgetMyCart> {
+  List<Cart> listMyCart = [];
+
+  @override
+  void initState() {
+    listMyCart
+      ..add(Cart(
+          productName: 'Haicila Two Tone V-neck Blouse',
+          price: 89000,
+          size: 'M',
+          quantity: 1,
+          color: 'Black',
+          image: 'assets/images/img_item_cart_1.jpeg'))
+      ..add(Cart(
+          productName: 'Y&F Samhita Plain Bodycon',
+          price: 159000,
+          size: 'L',
+          quantity: 1,
+          color: 'Maroon',
+          image: 'assets/images/header_1.jpg'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
+    return BackdropFilter(
+      filter: ImageFilter.blur(
+        sigmaX: 5.0,
+        sigmaY: 5.0,
+      ),
+      child: Container(
+        height: mediaQuery.height / 2.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(48.0),
+            topRight: Radius.circular(48.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 24.0,
+            top: 40.0,
+            right: 24.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            children: <Widget>[
+              _buildWidgetHeaderMyCart(context),
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: Divider(
+                  height: 2.0,
+                  color: Colors.grey,
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  itemCount: listMyCart.length,
+                  itemBuilder: (context, index) {
+                    Cart cart = listMyCart[index];
+                    String rupiahPrice =
+                        NumberFormat('#,##0', 'id_ID').format(cart.price);
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image.asset(
+                            cart.image,
+                            width: 48.0,
+                            height: 48.0,
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: Wrap(
+                              direction: Axis.vertical,
+                              children: <Widget>[
+                                Text(
+                                  cart.productName,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .body1
+                                      .merge(TextStyle(
+                                          fontWeight: FontWeight.w500)),
+                                ),
+                                Text(
+                                  'Rp $rupiahPrice',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .body1
+                                      .merge(TextStyle(fontSize: 12.0)),
+                                ),
+                                Text(
+                                  'Size: ${cart.size}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .body1
+                                      .merge(TextStyle(
+                                        fontSize: 12.0,
+                                        color: Colors.grey,
+                                      )),
+                                ),
+                              ],
+                            ),
+                          ),
+                          WidgetControllerQuantity(cart.quantity),
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      height: 2.0,
+                      color: Colors.grey,
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Divider(
+                  thickness: 2.0,
+                  color: Colors.black,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Total amount',
+                      style: Theme.of(context).textTheme.body1.merge(
+                            TextStyle(color: Colors.grey),
+                          )),
+                  Text('Rp 248.000',
+                      style: Theme.of(context).textTheme.body1.merge(
+                            TextStyle(fontWeight: FontWeight.w600),
+                          )),
+                ],
+              ),
+              SizedBox(height: 8.0),
+              SizedBox(
+                width: mediaQuery.width,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Check out',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                  color: Color(0xFF32312D),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetHeaderMyCart(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'My Cart',
+          style: Theme.of(context).textTheme.body1.merge(TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16.0,
+              )),
+        ),
+        Text('2 items', style: Theme.of(context).textTheme.caption),
+      ],
+    );
+  }
+}
+
+class WidgetControllerQuantity extends StatefulWidget {
+  final int qty;
+
+  WidgetControllerQuantity(this.qty);
+
+  @override
+  _WidgetControllerQuantityState createState() =>
+      _WidgetControllerQuantityState();
+}
+
+class _WidgetControllerQuantityState extends State<WidgetControllerQuantity> {
+  int qty;
+
+  @override
+  void initState() {
+    qty = widget.qty;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            if (qty == 1) return;
+            setState(() {
+              qty -= 1;
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Icon(
+              Icons.remove,
+              size: 20.0,
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        Text('$qty'),
+        GestureDetector(
+          onTap: () {
+            if (qty == 10) return;
+            setState(() {
+              qty += 1;
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Icon(
+              Icons.add,
+              size: 20.0,
+              color: Colors.black54,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Cart {
+  String productName;
+  int price;
+  String size;
+  int quantity;
+  String color;
+  String image;
+
+  Cart(
+      {this.productName,
+      this.price,
+      this.size,
+      this.quantity,
+      this.color,
+      this.image});
 }
